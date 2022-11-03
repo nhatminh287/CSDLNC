bd5ad59ae321003e5b28080f6eb72116bd73793a
nhatminh287 <nhatminhpro2001@gmail.com>
2022-11-03 13:07:38 +0700

    commit

--- models/dbConfig.js
+++ models/dbConfig.js
@@ -1,15 +1,22 @@
 const sql = require('mssql/msnodesqlv8');
 
 const config = {
-    user: 'PCT',
+    /* user: 'PCT',
     password: '1234',
     server: 'CONGTUAN\\CONGTUAN',
     driver: 'msnodesqlv8',
     database: 'DoAn02',
-    port: 1433,
+    port: 1433, */
     options: {
         trustedConnection: true
-    }
+    },
+    
+    server: "localhost",
+    user: "sa",
+    password: "1234",
+    database: "DoAn02",
+    DRIVER: "msnodesqlv8"
+    
 }
 
 async function connect(){
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 
 const siteRoute = require('./site');
-const productsRouter = require('./products');
+// const productsRouter = require('./products');
 const ordersRoute = require('./orders');
 const adminRoute = require('./admin');
 const managerRoute = require('./manager');
@@ -9,7 +9,7 @@ function route(app) {
     app.use('/manager', managerRoute);
     app.use('/admin', adminRoute);
     app.use('/my-orders', ordersRoute);
-    app.use('/products', productsRouter);
+    // app.use('/products', productsRouter);
     app.use('/', siteRoute);
 }
 module.exports = route;
